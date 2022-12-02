c6791b3e0c1c7cbd577a04f53985f804ef7a5a26
Doshijainam <Jdoshi3117@conestogac.on.ca>
2022-12-01 20:10:24 -0500

    Volume and Engine Time Implemented!

---
+++ CSCN72030_Group05_CMS/volumeAndEngineTime.cpp
@@ -0,0 +1,137 @@
+
+
+#pragma once
+
+#include<iostream>
+#include<ctime>
+#include<fstream>
+#include<string>
+#include<ctime>
+#include<cstdio>
+#include "volumeAndEngineTime.h"
+using namespace std;
+
+
+
+string Volume::incrementVolume(string currentVolume)
+{
+	string line;
+	ifstream file;
+	file.open("volumeLevels.txt", ios::in);
+	while (!file.eof())
+	{
+		getline(file, line);
+		if (line == currentVolume)
+		{
+			getline(file, line);
+			//cout << line;
+			return line;
+		}
+	}
+	file.close();
+
+}
+
+string Volume::decrementVolume(string currentVolume)
+{
+	string line;
+	fstream file;
+	fstream temp;
+	file.open("volumeLevels.txt", ios::in);
+	temp.open("temp.txt", ios::out);
+
+	while (getline(file, line))
+	{
+		if (line != currentVolume)
+		{
+			temp << line << endl;
+		}
+		if (line == currentVolume)
+		{
+			break;
+		}
+	}
+
+	while (!temp.eof())
+	{
+		getline(temp, line);
+	}
+
+	return line;
+
+	file.close();
+	temp.close();
+
+}
+
+void engineTime::setEngineTime(string engineTime)
+{
+	//clock function to start timer - need buttonClick event for this to work
+		//float c_start = clock();
+		//......
+		//......
+		//float c_end = clock();
+		//engineTime = c_end - c_start; //This is the engine time usage
+	fstream file;
+	file.open("engineTime.txt", ios::out);
+	if (file.is_open())
+	{
+		file << engineTime << endl;
+	}
+	file.close();
+}
+
+string engineTime::getEngineTime()
+{
+	fstream file;
+	string line;
+	file.open("engineTime.txt", ios::in);
+	while (!file.eof())
+	{
+		getline(file, line);
+	}
+	return line;
+	file.close();
+
+}
+
+string engineTime::getAverageEngineTime()
+{
+	fstream file;
+	string line, result;
+	file.open("Duration.txt", ios::in);
+	int total = 0;
+	double average, sum = 0;
+	while (getline(file, line))
+	{
+		sum += stod(line);
+		total++;
+	}
+
+	average = sum / total;
+	result = to_string(average);
+	file.close();
+	return result;
+}
+
+void engineTime::setaverageEngineTime(string duration)
+{
+	string line;
+	fstream file;
+	file.open("Duration.txt", ios::app);
+	file << duration << endl;
+	file.close();
+}
+
+bool engineTime::alertEngineTime(string averageEngineTime)
+{
+	double avgtime = stod(averageEngineTime);
+	if (avgtime > 10)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
---
+++ CSCN72030_Group05_CMS/volumeAndEngineTime.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+#include<ctime>
+#include<fstream>
+#include<string>
+#include<ctime>
+#include<cstdio>
+using namespace std;
+
+class Volume
+{
+public:
+	string incrementVolume(string currentVolume);
+	string decrementVolume(string currentVolume);
+};
+
+class engineTime
+{
+public:
+	void setEngineTime(string engineTime);
+	string getEngineTime();
+	void setaverageEngineTime(string duration);
+	string getAverageEngineTime();
+	bool alertEngineTime(string averageEngineTime);
+}; 
